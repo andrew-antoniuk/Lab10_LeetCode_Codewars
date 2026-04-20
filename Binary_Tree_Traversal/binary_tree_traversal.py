5b0ad453063499cc8fe3616a9e284373df265f4e
andrew-antoniuk <a.antoniuk.pn@ucu.edu.ua>
2026-04-20 19:18:55 +0300

    Add preorder traversal

--- Binary_Tree_Traversal/binary_tree_traversal.py
+++ Binary_Tree_Traversal/binary_tree_traversal.py
@@ -1,3 +1,50 @@
 """
-Descr
+Docstring for Binary_Tree_Traversal.binary_tree_traversal
 """
+
+class Node:
+    def __init__(self, v):
+        self.left = None
+        self.right = None
+        self.data = v
+
+# Pre-order traversal
+def pre_order(node):
+
+    """
+    Docstring for pre_order
+    """
+
+    nodes = []
+
+    if node is None:
+        return None
+
+    # Visit Node
+    nodes.append(node.data)
+
+    # Traverse left subtree
+    pre_order(node.left)
+
+    # Traverse right subtree
+    pre_order(node.right)
+
+    return nodes
+
+# In-order traversal
+def in_order(node):
+
+    """
+    Docstring for in_order
+    """
+
+    return []
+
+# Post-order traversal
+def post_order(node):
+
+    """
+    Docstring for post_order
+    """
+
+    return []
